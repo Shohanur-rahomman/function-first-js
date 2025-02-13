b23d13693766fa56632d43e24f79526af97e88fa
Shohanur-rahomman <mdshohanur757@gmail.com>
2025-02-13 23:28:41 +0600

    first commit

---
+++ function-1.js
@@ -0,0 +1,27 @@
+function milesToKilometer(miles) {
+    const kilometer = miles * 1.60934;
+    return kilometer;
+}
+
+const result = milesToKilometer(5);
+console.log(result, 'km');
+
+function miles(km) {
+    const milesKm = km * 0.621371;
+    return milesKm;
+}
+
+const resultMiles = miles(50);
+// console.log(resultMiles, 'miles');
+
+
+function rollsOfInch (inch){
+    const fitToInch = inch / 12;
+    const fitNumber = parseInt(fitToInch);
+    const fitInch = inch % 12;
+    const result = fitNumber + ' fit ' + fitInch + ' inch';
+    return result; 
+}
+
+const joy = rollsOfInch(75);
+console.log(joy);
