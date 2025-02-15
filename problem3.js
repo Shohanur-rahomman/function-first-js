83ba1c5fbb91021f5f2f7dbe6d3570afe8140c44
Shohanur-rahomman <mdshohanur757@gmail.com>
2025-02-15 18:54:27 +0600

    new update

---
+++ problem3.js
@@ -0,0 +1,34 @@
+function stringValidation(name) {
+    if (typeof name !== 'string') {
+        return 'Invalid Input';
+    }
+
+    let character = false;//why this one asaind out sit
+    for (let i = 0; i < name.length; i++) {
+        let char = name[i];//why i use for loop insitd why no assind out sit
+        if (!isNaN(char) && char !== ' ') {
+            character = true;
+            break;//why i use break if i use return then what happen
+        }
+    }
+    return character;
+}
+const result = stringValidation();
+console.log(result);
+
+function userNameValidation(name) {
+    if (typeof name !== 'string') {
+        return 'Invalid Input';
+    }
+    
+    for (let i = 0; i < name.length; i++) {
+        const cher = name[i];
+        if (!isNaN(cher) && cher !== '') {
+            return true;  
+        }
+    }
+    return false;
+}
+
+const result1 = userNameValidation('');
+console.log(result1);
